0a22bf794037a08ecb14587787a03348cfa19880
Tamara Sejic <sejic.tamara@gmail.com>
2023-10-19 19:11:48 +0200

    Add TC_ST_01 Visits the signup page

--- src/test/java/tests/SignupTests.java
+++ src/test/java/tests/SignupTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import retry.RetryAnalyzer;
@@ -11,6 +12,15 @@ public class SignupTests extends BasicTest{
         Assert.assertEquals(driver.getCurrentUrl(),
                 baseUrl + "/",
                 "Url should be " + baseUrl  + "/");
+    }
+
+    @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void visitsTheSignUpPage() {
+        navPage.clickOnSignupButton();
+
+        wait
+                .withMessage("Url should be '" + baseUrl + "/signup")
+                .until(ExpectedConditions.urlToBe(baseUrl + "/signup"));
     }
 
 }
