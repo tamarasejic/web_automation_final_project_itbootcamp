a61a9a9bcefce83e1bc374ca678bfb66f4dbaf4c
Tamara Sejic <sejic.tamara@gmail.com>
2023-10-21 03:17:06 +0200

    Add TC_LOT_00 Verify the url - for LocaleTests

---
+++ src/test/java/tests/LocaleTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import retry.RetryAnalyzer;
+
+public class LocaleTests extends BasicTest{
+
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void verifyTheUrl() {
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/",
+                "Url should be " + baseUrl  + "/");
+    }
+
+}
