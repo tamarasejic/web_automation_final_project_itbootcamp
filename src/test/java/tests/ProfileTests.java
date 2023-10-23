92defc4ac15e3fdc15c2ac41bccedd4586f7686d
Tamara Sejic <sejic.tamara@gmail.com>
2023-10-23 23:18:40 +0200

    TC_PT_00 Verify the url - for ProfileTests

---
+++ src/test/java/tests/ProfileTests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import retry.RetryAnalyzer;
+
+public class ProfileTests extends BasicTest{
+
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void verifyTheUrl() {
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/",
+                "Url should be " + baseUrl  + "/");
+    }
+}
